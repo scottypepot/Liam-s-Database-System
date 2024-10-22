ddbd1ae693a65ffa5b7e82a7e581181a5a9fa6d3
scott <scottbenz03@gmail.com>
2024-10-23 02:14:46 +0800

    added models

--- feedcomments/models.py
+++ feedcomments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    feedback = models.CharField(max_length=500)
+    comments = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.feedback
