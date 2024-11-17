b7987f873cb7a80c54b410ae8833c72dbe3544ca
calimps115646 <jdcalimpong5646@gmail.com>
2024-11-18 06:35:09 +0800

    updated the UI of apply_leave.html

--- leave/forms.py
+++ leave/forms.py
@@ -18,3 +18,13 @@ class LeaveForm(forms.ModelForm):
             'start_date': forms.DateInput(attrs={'type': 'date'}),
             'end_date': forms.DateInput(attrs={'type': 'date'}),
         }
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+
+        if start_date and end_date:
+            if start_date > end_date:
+                raise forms.ValidationError("The 'From' date cannot be later than the 'To' date.")
+        return cleaned_data
--- leave/views.py
+++ leave/views.py
@@ -1,4 +1,3 @@
-# views.py
 from django.shortcuts import render, redirect
 from .models import Leave
 from .forms import LeaveForm
@@ -9,8 +8,7 @@ def leave_list(request):
     leave_details = []
 
     for leave in leaves:
-        # Calculate the number of days
-        num_days = (leave.end_date - leave.start_date).days + 1  # Add 1 to include the start day
+        num_days = (leave.end_date - leave.start_date).days + 1 
         leave_details.append((leave, num_days))
 
     return render(request, 'leave/leave_list.html', {'leave_details': leave_details})
@@ -20,7 +18,7 @@ def apply_leave(request):
         form = LeaveForm(request.POST)
         if form.is_valid():
             leave = form.save(commit=False)
-            leave.user = None  # Set to None for anonymous submissions
+            leave.user = None  
             leave.save()
             return redirect('leave_list')
     else:
