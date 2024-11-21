083ba45a9118a638d5729b29d5336855f35485b0
scottypepot <scottbenz03@gmail.com>
2024-11-22 01:21:24 +0800

    updated activity and feedcomments

--- activity_logs/views.py
+++ activity_logs/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Activity
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def activity_list(request):
     activities = Activity.objects.all().order_by('-timestamp')
     return render(request, 'activity_list.html', {'activities': activities})
 
+@login_required
 def add_activity(request):
     if request.method == 'POST':
-        user = request.POST['user']
-        message = request.POST['message']
-        Activity.objects.create(user=user, message=message, timestamp=timezone.now())
-        return redirect('activity_list')
+        user = request.user
+        message = request.POST.get('message')
+        activity = Activity(user=user, message=message)
+        activity.save()
+        return redirect('activity_list') 
+    
     return render(request, 'add_activity.html')
 
 def update_activity(request, id):
--- feedcomments/urls.py
+++ feedcomments/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.feedback_list, name='feedback_list'),
     path('add/', views.add_feedback, name='add_feedback'),
+    path('feedback/delete/<int:id>/', views.delete_feedback, name='delete_feedback'),
+    path('feedback/update/<int:id>/', views.update_feedback, name='update_feedback'),
 ]
