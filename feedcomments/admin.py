e4e8536b2a48b3e810f1671aa6936410cb059a73
haloimnotcode <johngerard.donaire@cit.edu>
2024-12-08 03:40:02 +0800

    updated the physical and logical data model

--- feedcomments/admin.py
+++ feedcomments/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Feedback, Comment
 
-# Register your models here.
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('user', 'comment', 'timestamp')
+    search_fields = ('user__username', 'comment')
+    list_filter = ('timestamp',)
+    ordering = ('-timestamp',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('feedback', 'user', 'message', 'timestamp')
+    search_fields = ('feedback__comment', 'user__username', 'message')
+    list_filter = ('timestamp',)
+    ordering = ('-timestamp',)
